c5782cf55050d3e815f0e0228a797c2b61380d22
Becky B <rbelisle@wellesley.edu>
2021-08-05 10:46:35 -0400

    attempts at fitting

--- conductivity_analysis.py
+++ conductivity_analysis.py
@@ -4,12 +4,12 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import scipy.stats
-import scipy.optimize 
+from scipy.optimize import curve_fit
 import pandas as pd
 
 #%%
 path = r'/Users/rbelisle/Desktop' # use your path
-data = path+'/MAPBBr2I_sample2_N2_1nA.csv'
+data = path+'/MAPBI3_N2_1nA.csv'
 measure = pd.read_csv(data,delimiter=',',header=0) #read file
 
 # %% Call out relevant variables
@@ -21,22 +21,27 @@ current = np.array(measure['Current [A]'])
 fig2,ax2 = plt.subplots()
 ax2.set_xlabel('Time [s]',size=14) #Define x-axis label
 ax2.set_ylabel('Voltage [V]',size=14)#Define y-axis label
-#ax2.set_ylim([0,0.02])
-ax2.set_xlim([-100,2000])
+#ax2.set_ylim([0,0.015])
+#ax2.set_xlim([0,3000])
+#ax2.plot(time2-50,voltage2-voltage2[500], 'ro--')
 plt.plot(time,voltage, 'ko--')
-plt.plot(time,current*10**6)
-
+#plt.plot(time,current, 'ro--')
 
 #%%
-def simple_model(t_, C, V0, Vinf):
-    Re=Vinf/Iin;
-    Ri=Vinf*V0/(Iin*(Vinf-V0));
-#     C=10^-7*5;
-    return (Ri*Re)/(Ri+Re)*Iin*np.exp(-t_/((Re+Ri)*C))-Re*Iin*(np.exp(-t_/((Re+Ri)*C))-1);
+def simple_model(t, Vo, tao):
+    return Vo*(1-np.exp(-t*tao))
 # %%
-Iin = current[100]
-p0 = [10**-7, .0035, .02]
-popt,pcov = curve_fit(simple_model, time[0:700], voltage[0:700], p0, maxfev=6000)
-plt.plot(time[0:700],voltage[0:700], 'ko--')
+
+p0 = (.0035, .02)
+popt,pcov = curve_fit(simple_model, time-10, voltage-voltage[100], p0, maxfev=6000)
+plt.plot(time-10,voltage-voltage[100], 'ko--')
+plt.plot(time,simple_model(time, *popt))
+print(popt[0], np.sqrt(np.diag(pcov))[0])
+# %%
+p0 = (.0035, .02)
+popt,pcov = curve_fit(simple_model, time2[501:1000]-50, voltage2[501:1000]-voltage2[500], p0, maxfev=6000)
+plt.plot(time2[0:1000]-50,voltage2[0:1000]-voltage2[500], 'ko--')
 plt.plot(time[0:900],simple_model(time[0:900], *popt))
+print(popt[0], np.sqrt(np.diag(pcov))[0])
+
 # %%
